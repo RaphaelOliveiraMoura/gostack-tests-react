a5ffacb05a3d80ef56e6b79f7c4a6009480fe2c1
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-21 11:45:45 -0300

    feat: Axios test mock

--- __tests__/store/sagas/techs.test.js
+++ __tests__/store/sagas/techs.test.js
@@ -1,13 +1,31 @@
 import { runSaga } from 'redux-saga';
-import { getTechsSuccess } from '~/store/modules/tech/actions';
+import MockAdapter from 'axios-mock-adapter';
+
+import api from '~/services/api';
+
+import { getTechsSuccess, getTechsFailure } from '~/store/modules/tech/actions';
 import { getTechs } from '~/store/modules/tech/sagas';
+
+const apiMock = new MockAdapter(api);
 
 describe('Techs saga', () => {
   it('should be able to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('/techs').reply(200, ['React JS']);
+
     await runSaga({ dispatch }, getTechs).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['React JS']));
+  });
+
+  it('should  fail when api return a error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('/techs').reply(500);
+
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   });
 });
