885906cbefd1584b6b41bfc8ae27b85d6e56ba64
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-21 11:53:25 -0300

    feat: Coverage tests report

--- __tests__/store/reducers/techs.test.js
+++ __tests__/store/reducers/techs.test.js
@@ -2,6 +2,12 @@ import reducer, { INITIAL_STATE } from '~/store/modules/tech/reducer';
 import * as Techs from '~/store/modules/tech/actions';
 
 describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('ADD_TECH', () => {
     const state = reducer(INITIAL_STATE, Techs.addTech('React JS'));
 
